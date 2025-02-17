0c360bc0f537db233fc4fd5cf133b7330d8c3500
Manuel <000balderas@gmail.com>
2025-02-17 09:43:46 -0600

    Actualizacion GUIA biologia

---
+++ pages/test.js
@@ -0,0 +1,16 @@
+let main = document.querySelector(".content");
+
+const newQuestion = (indice) => {
+    // NUMERACION
+    // PREGUNTA
+    // puntos
+    // RESPUESTA 1 = Correcta
+    // RESPUESTA 2
+    // RESPUESTA 3
+    // RESPUESTA 4
+    let quest = document.createElement("h2");
+    let 
+    //indice.appendChild();
+};
+let random = Math.floor(Math.random() * 6) + 1
+document.body.innerHTML = random;
