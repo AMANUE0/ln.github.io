4d098de2caf94491f99763db8ccf764a1e529e75
Manuel <000balderas@gmail.com>
2025-04-06 21:40:27 -0600

    Actualizacion etapa 3

--- v0/sidebar.js
+++
@@ -1,6 +0,0 @@
-let menuButton = document.querySelector(".menu-button");
-let sidebar = document.querySelector(".sidebar");
-menuButton.addEventListener("click", () => {
-  menuButton.classList.toggle("menu-button-active");
-  sidebar.classList.toggle("sidebar-disable");
-});
